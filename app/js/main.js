6d27b1f68e7e3bbd36a1c42db3f518bd966321c9
Denis <kdb2015@mail.ru>
2019-12-02 17:12:34 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(function(){
 
+//rateYo
+$("#rateYo").rateYo({
+    starWidth: "17px",
+    rating: 4.5,
+    readOnly: true
+  });
+
+//slick
+$('.slideProducts__inner').slick({
+	dots: false,
+	slidesToShow: 1,
+ 	slidesToScroll:1,
+ 	arrows: true,
+ 	prevArrow: '<div class="prev icon-angle-left"></div>',
+  	nextArrow: '<div class="next icon-angle-right"></div>',
+});
+
 });
